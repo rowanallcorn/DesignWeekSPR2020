e04154e13dc5bad3fb7a67a61764ad1004ccafbd
GabrielHoltschlag <gabrielholtschlag@gmail.com>
2020-03-02 16:49:02 -0500

    Get Player MovementInput

---
+++ DesignWeek_Spring2020/Assets/Scripts/scr_PlayerInput_Component.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_PlayerInput_Component : MonoBehaviour
+{
+    //Get movement input
+    public Vector2 GetMovementInput(KeyCode horNegativeKey, KeyCode horPositiveKey, KeyCode verNegativeKey, KeyCode verPositiveKey)
+    {
+        float horNeg = Input.GetKey(horNegativeKey) ? -1 : 0;
+        float horPos = Input.GetKey(horPositiveKey) ? 1 : 0;
+        float horInput = horNeg + horPos;//horizontal input
+        float verNeg = Input.GetKey(verNegativeKey) ? -1 : 0;
+        float verPos = Input.GetKey(verPositiveKey) ? 1 : 0;
+        float verInput = verNeg + verPos;//vertical input
+        return new Vector2(horInput,verInput);//final input
+    }
+
+}
---
+++ DesignWeek_Spring2020/Assets/Scripts/scr_Player_Controller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_Player_Controller : MonoBehaviour
+{
+    private scr_PlayerInput_Component s_PlayerInput_Component;
+    [SerializeField] private Vector2 movementInput;
+    [SerializeField] private string upKey, downKey, leftKey, rightKey;
+    void Start()
+    {
+        s_PlayerInput_Component = GetComponent<scr_PlayerInput_Component>();
+    }
+
+    void Update()
+    {
+        //GetMovement Input
+        movementInput = s_PlayerInput_Component.GetMovementInput(
+            (KeyCode)System.Enum.Parse(typeof(KeyCode), leftKey),
+            (KeyCode)System.Enum.Parse(typeof(KeyCode), rightKey),
+            (KeyCode)System.Enum.Parse(typeof(KeyCode), downKey),
+            (KeyCode)System.Enum.Parse(typeof(KeyCode), upKey));
+    }
+}
