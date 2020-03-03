2aee2bdd452731c4daa01dffb787a89bfe4ee58a
GabrielHoltschlag <gabrielholtschlag@gmail.com>
2020-03-03 10:36:32 -0500

    Re setup gamespace

--- DesignWeek_Spring2020/Assets/Scripts/scr_SproutController.cs
+++ DesignWeek_Spring2020/Assets/Scripts/scr_SproutController.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class scr_SproutController : MonoBehaviour
 {
     [SerializeField] GameObject sproutObstaclePrefab;
+    bool active;
     public void Activate()//called when sprout is watered
-    {  StartCoroutine(wateredActions());}
+    {
+        if (!active)
+        { StartCoroutine(wateredActions()); }
+    }
     IEnumerator wateredActions()
     {
+        active = true;
         print(gameObject.name + " was wattered");
         yield return new WaitForSeconds(.2f);//wait a bit
         Instantiate(sproutObstaclePrefab, transform.position, transform.rotation);//instatiate wall
-        Destroy(gameObject);//die
+        //Destroy(gameObject);//die
     }
 
 }
