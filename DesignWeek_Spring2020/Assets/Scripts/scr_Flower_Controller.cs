ef2f570a25ef4a563a02da2c9206e6bdf027801c
GabrielHoltschlag <gabrielholtschlag@gmail.com>
2020-03-03 13:23:13 -0500

    Idk

---
+++ DesignWeek_Spring2020/Assets/Scripts/scr_Flower_Controller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_Flower_Controller : MonoBehaviour
+{
+    [SerializeField] private float minY, maxY;
+    private bool movingDown;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (movingDown)
+        { 
+        
+        }
+    }
+}
