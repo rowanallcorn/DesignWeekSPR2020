7318938d10e80309695ebc2724b658f1a7cb35ac
GabrielHoltschlag <gabrielholtschlag@gmail.com>
2020-03-02 20:15:23 -0500

    Start Sprout Obstacle Controller script (mid development)

---
+++ DesignWeek_Spring2020/Assets/Scripts/scr_SproutObstacle_Controller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_SproutObstacle_Controller : MonoBehaviour
+{
+    [SerializeField] private float health;
+    public void takeDamage()//called from bullet
+    {
+        health -= 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
