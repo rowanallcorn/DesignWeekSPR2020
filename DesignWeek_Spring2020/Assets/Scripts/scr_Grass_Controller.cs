4216193f9041a4c060cf2d92f0139ed7cdfc38fb
GabrielHoltschlag <gabrielholtschlag@gmail.com>
2020-03-03 14:52:33 -0500

    Add Grass tiles

---
+++ DesignWeek_Spring2020/Assets/Scripts/scr_Grass_Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_Grass_Controller : MonoBehaviour
+{
+    public bool isActive;
+    public void Activate(GameObject spawnableObj)//called when sprout is watered
+    {
+        if (!isActive)
+        { StartCoroutine(wateredActions(spawnableObj)); }
+    }
+    IEnumerator wateredActions(GameObject spawnableObj)
+    {
+        isActive = true;
+        //print(gameObject.name + " was wattered");
+        yield return new WaitForSeconds(.2f);//wait a bit
+        GameObject newSpawnable = Instantiate(spawnableObj, transform.position, transform.rotation);//instatiate wall
+        newSpawnable.GetComponent<scr_DeathSproutReset_Controller>().originSprout = gameObject;
+        //Destroy(gameObject);//die
+    }
+}
---
+++ DesignWeek_Spring2020/Assets/Scripts/scr_Reference_Manager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_Reference_Manager : MonoBehaviour
+{
+    [SerializeField] private GameObject barrierPrefabRef, turretPrefabRef;
+    public static GameObject barrierPrefab, turretPrefab;
+    void Awake()
+    {
+        barrierPrefab = barrierPrefabRef;
+        turretPrefab = turretPrefabRef;
+    }
+}
