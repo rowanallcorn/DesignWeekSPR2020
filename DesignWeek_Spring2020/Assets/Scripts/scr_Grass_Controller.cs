bcd072515e1004963dad64d566990d860c65dea7
GabrielHoltschlag <gabrielholtschlag@gmail.com>
2020-03-04 00:52:08 -0500

    Implement Grass regeneration

--- DesignWeek_Spring2020/Assets/Scripts/scr_Grass_Controller.cs
+++ DesignWeek_Spring2020/Assets/Scripts/scr_Grass_Controller.cs
@@ -5,19 +5,40 @@ using UnityEngine;
 public class scr_Grass_Controller : MonoBehaviour
 {
     public bool isActive;
+    public bool isGrassy=true;
+    private bool isDead;
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
     public void Activate(GameObject spawnableObj)//called when sprout is watered
     {
-        if (!isActive)
+        if (!isActive&&isGrassy)
         { StartCoroutine(wateredActions(spawnableObj)); }
     }
     IEnumerator wateredActions(GameObject spawnableObj)
     {
-        
+        isGrassy = false;
         isActive = true;
+        isDead = true;
         //print(gameObject.name + " was wattered");
         yield return new WaitForSeconds(.2f);//wait a bit
         GameObject newSpawnable = Instantiate(spawnableObj, transform.position, transform.rotation);//instatiate wall
         newSpawnable.GetComponent<scr_DeathSproutReset_Controller>().originSprout = gameObject;
         //Destroy(gameObject);//die
+    }
+    private void Update()
+    {
+        if (!isGrassy && !isActive&& isDead)
+        {
+            isDead = false;
+            anim.SetTrigger("GrowBack");
+        }
+    }
+    public void RegenerateGrass()
+    {
+        isGrassy = true;
     }
 }
