aa9abe4e904bd77c1ab85491c92b9677ab435197
GabrielHoltschlag <gabrielholtschlag@gmail.com>
2020-03-04 00:12:23 -0500

    Add dynamic sorting order

---
+++ DesignWeek_Spring2020/Assets/Scripts/scr_SpriteOrder_Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_SpriteOrder_Controller : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    private float newSortingOrder;
+    [SerializeField] private bool runOnUpdate;
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        SetSortingORder();
+    }
+    void Update()
+    {
+        if (runOnUpdate)
+        { SetSortingORder(); }
+    }
+    void SetSortingORder()
+    {
+        newSortingOrder = (-transform.position.y + 100) * 100f - 9000;
+        sr.sortingOrder = ((int)newSortingOrder);
+    }
+}
