cc72b856746605a265937ca316bd90271f9336c7
GabrielHoltschlag <gabrielholtschlag@gmail.com>
2020-03-04 18:25:23 -0500

    Implement Barrrier animations

--- DesignWeek_Spring2020/Assets/Scripts/scr_SproutObstacle_Controller.cs
+++ DesignWeek_Spring2020/Assets/Scripts/scr_SproutObstacle_Controller.cs
@@ -6,25 +6,20 @@ public class scr_SproutObstacle_Controller : MonoBehaviour
 {
     [SerializeField] private float health;
     scr_DeathSproutReset_Controller s_DeathSproutReset_Controller;
-    [SerializeField] private List<Sprite> sprites;
-    private SpriteRenderer sr;
+    private Animator anim;
 
     private void Start()
     {
         s_DeathSproutReset_Controller = GetComponent<scr_DeathSproutReset_Controller>();
-        sr = GetComponent<SpriteRenderer>();
-        ChangeSprite();
+        anim = GetComponent<Animator>();
     }
     public void takeDamage()//called from bullet
     {
         health -= 1;
-        ChangeSprite();
-    }
-    void ChangeSprite()
-    {
         if (health > 0)
-        { sr.sprite = sprites[(int)health - 1]; }
+        { anim.SetTrigger("Hit"); }
     }
+
 
     void Update()
     {
