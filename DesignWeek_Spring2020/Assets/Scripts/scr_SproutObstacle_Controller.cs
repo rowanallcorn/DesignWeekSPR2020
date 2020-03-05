16330c2cae5a33bb3b67303c21f7b32aca711829
GabrielHoltschlag <gabrielholtschlag@gmail.com>
2020-03-05 15:31:57 -0500

    Implement barrier decay

--- DesignWeek_Spring2020/Assets/Scripts/scr_SproutObstacle_Controller.cs
+++ DesignWeek_Spring2020/Assets/Scripts/scr_SproutObstacle_Controller.cs
@@ -7,7 +7,8 @@ public class scr_SproutObstacle_Controller : MonoBehaviour
     [SerializeField] private float health;
     scr_DeathSproutReset_Controller s_DeathSproutReset_Controller;
     private float waterGunned;
-
+    [SerializeField] private float lifeInSeconds;
+    private bool wasShot;
     private Animator anim;
 
 
@@ -18,21 +19,32 @@ public class scr_SproutObstacle_Controller : MonoBehaviour
     }
     void Update()
     {
-        if (health <= 0)
-        { s_DeathSproutReset_Controller.Die(); }
+        if (health <= 0&&!wasShot)
+        { StartCoroutine(Die(0)); }
+        waterGunned+= 1/ lifeInSeconds * Time.deltaTime;
+        if (health == 3 && waterGunned > .33f) DieABit();
+        if (health == 2 && waterGunned > .66f) DieABit();
+        if (health == 1 && waterGunned > .99f) DieABit();
     }
     public void takeDamage()//called from bullet
-    {
-        waterGunned += 1 * Time.deltaTime;
-        if (health == 3&&waterGunned>.1f) DieABit();
-        if (health == 2 && waterGunned > .62f) DieABit();
-        if (health == 1 && waterGunned > .98f) DieABit();
+    {  waterGunned += 1 * Time.deltaTime;
+        if (!wasShot)
+        {
+            wasShot = true;
+            StartCoroutine(Die(1));
+        }
+
     }
     void DieABit()
     {
         health -= 1;
         if (health > 0)
         { anim.SetTrigger("Hit"); }
+    }
+    IEnumerator Die(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        s_DeathSproutReset_Controller.Die();
     }
 
 
