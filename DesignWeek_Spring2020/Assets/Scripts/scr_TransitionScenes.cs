da6e37c70749a933c29c5ce2d5e3b5e8a5e40c01
GabrielHoltschlag <gabrielholtschlag@gmail.com>
2020-03-05 16:57:07 -0500

    Fix conflict

---
+++ DesignWeek_Spring2020/Assets/Scripts/scr_TransitionScenes.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scr_TransitionScenes : MonoBehaviour
+{
+    [SerializeField] private bool onAnyKey;
+    [SerializeField] private int newScene;
+    private Animator anim;
+    private bool transitioned;
+
+    private void Start()
+    {  anim = GetComponent<Animator>();  }
+    void Update()
+    {
+        if (onAnyKey)
+        {
+            if (Input.anyKey&& !transitioned)
+            { 
+                anim.SetTrigger("Transition");
+                transitioned = true;
+            }
+        }
+    }
+    public void LoadNewScene()
+    {
+        SceneManager.LoadScene(newScene);
+    }
+}
